2f2e59e8e6924eedf50929a3bfe884c80bb457bf
sheha <shehabeldenehab@gmail.com>
2024-07-21 00:46:27 +0300

    add the logo

--- lib/core/utils/design_utils/app_images.dart
+++ lib/core/utils/design_utils/app_images.dart
@@ -1,3 +1,7 @@
 abstract final class AppAnimatedImages{
   static const String noDataAnimation = 'assets/animated_jsons/no_data_animation.json';
 }
+
+abstract final class AppImages{
+  static const String logo = 'assets/images/logo.png';
+}

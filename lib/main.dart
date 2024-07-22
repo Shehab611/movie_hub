689efff6c8f3e38bf3f0632a90c4ff98e40f090e
sheha <shehabeldenehab@gmail.com>
2024-07-22 15:44:58 +0300

    add firebase to the project and upgrade gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/service_locator.dart';
 import 'core/utils/app_constants/app_strings.dart';
 import 'core/utils/app_routes_utils/app_router.dart';
 import 'core/utils/design_utils/app_theme.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServicesLocator();
   await sl<AppLanguage>().fetchLocale();
   await sl<AppTheme>().fetchTheme();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Semantics(
     key: const Key('Movie Hub Semantic Key'),
     label: 'Movie Hub App',

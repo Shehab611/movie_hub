c91574684b1726034775c6839c1b080b1f74fbba
sheha <shehabeldenehab@gmail.com>
2024-07-23 08:57:36 +0300

    edit in base use case

--- lib/core/base_use_case/base_use_case.dart
+++ lib/core/base_use_case/base_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:movie_hub/core/utils/api_utils/data_response.dart';
 
-abstract interface class BaseUseCaseInterface<T,P> {
-  Future<DataResponse<T>> call(P p);
+abstract interface class BaseUseCaseInterface<T extends Parameters> {
+  Future<DataResponse> call(T parameters);
 }

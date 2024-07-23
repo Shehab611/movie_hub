e4c0947eca114fcc5aae0c769fd2009c80edac7b
sheha <shehabeldenehab@gmail.com>
2024-07-23 09:33:50 +0300

    add social apps data layer in shared folder in authentication feature

---
+++ lib/features/authentication/shared/data_repositories/social_apps_sign_in_repository.dart
@@ -0,0 +1,21 @@
+import 'package:movie_hub/core/usable_functions/firebase/firebase_handling.dart';
+import 'package:movie_hub/core/utils/api_utils/data_response.dart';
+import 'package:movie_hub/features/authentication/shared/data_sources/social_apps_sign_in_remote_data_source.dart';
+import 'package:movie_hub/features/authentication/shared/domain_repositories/social_apps_sign_in_interface.dart';
+
+final class SocialAppsSignInRepository implements SocialAppsSignInInterface {
+  final SocialAppsSignInRemoteDataSource _dataSource;
+
+  const SocialAppsSignInRepository(this._dataSource);
+
+  @override
+  Future<DataResponse> googleSignIn() async {
+    try {
+      final FirebaseAuthResponse data = await _dataSource.googleSignIn();
+      return SuccessDataResponse<SuccessFirebaseAuthResponse>(
+          data as SuccessFirebaseAuthResponse);
+    } on FailureFirebaseAuthResponse catch (e) {
+      return FailureDataResponse<FailureFirebaseAuthResponse>(e);
+    }
+  }
+}
